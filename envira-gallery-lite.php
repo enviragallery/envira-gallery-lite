78ff68b0320b2daf3d8ae15d2e31d3a6c16c3772
David Bisset <dbisset@dimensionmedia.com>
2020-03-19 13:35:10 -0400

    Bump Version

--- envira-gallery-lite.php
+++ envira-gallery-lite.php
@@ -5,7 +5,7 @@
  * Description: Envira Gallery is the best responsive WordPress gallery plugin. This is the Lite version.
  * Author:      Envira Gallery Team
  * Author URI:  http://enviragallery.com
- * Version:     1.7.8.2
+ * Version:     1.7.8.3
  * Text Domain: envira-gallery-lite
  *
  * Envira Gallery is free software: you can redistribute it and/or modify
@@ -53,7 +53,7 @@ class Envira_Gallery_Lite {
 	 *
 	 * @var string
 	 */
-	public $version = '1.7.8.2';
+	public $version = '1.7.8.3';
 
 	/**
 	 * The name of the plugin.
@@ -80,7 +80,7 @@ class Envira_Gallery_Lite {
 	 *
 	 * @var string
 	 */
-	public $file = __FILE__;
+	public $file = false; // __FILE__;
 
 	/**
 	 * Primary class constructor.
